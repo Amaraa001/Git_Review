744c64c5878db32e59e0d3e728d8dc80fbc93765
Amaraa001 <107004297+Amaraa001@users.noreply.github.com>
2022-12-25 10:25:36 -0600

    Update A.java

--- src/part3/A.java
+++ src/part3/A.java
@@ -4,6 +4,13 @@ public class A {
     public static void main(String[] args) {
         System.out.println("Class A ");
         System.out.println("Class A1");
+         System.out.println("Class A1");
+         System.out.println("Class A1");
+         System.out.println("Class A1");
+         System.out.println("Class A1");
+         System.out.println("Class A1"); 
+        System.out.println("Class A1"); S
+            ystem.out.println("Class A1");
     }
 
 }
