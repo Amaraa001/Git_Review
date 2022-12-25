617434c2893f81ecfb55905be9c1e6d8f5aa26b2
amara <amarsaikhan.n@gmail.com>
2022-12-25 10:00:11 -0600

    Class4_1 changes

--- src/part4/Class4_1.java
+++ src/part4/Class4_1.java
@@ -1,6 +1,14 @@
 package part4;
 
 public class Class4_1 {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world1");
+        System.out.println("Hello world2");
+        System.out.println("Hello world3");
+        System.out.println("Hello world4");
+
+    }
 
 
 }
