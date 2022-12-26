adf5371f20b796235b9079cab3c7c0840b8211ac
amara <amarsaikhan.n@gmail.com>
2022-12-26 13:23:07 -0600

    created class5_1&2

---
+++ src/part5/Class5_1.java
@@ -0,0 +1,7 @@
+package part5;
+
+public class Class5_1 {
+    public static void main(String[] args) {
+        System.out.println("Class 5_1");
+    }
+}
---
+++ src/part5/Class5_2.java
@@ -0,0 +1,7 @@
+package part5;
+
+public class Class5_2 {
+    public static void main(String[] args) {
+        System.out.println("Class5_2");
+    }
+}
